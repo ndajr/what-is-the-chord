63250457f9394ed308581223f88d0f3379d94a93
Felipe Leão <felipe@felipeleao.com>
2018-05-14 13:11:14 -0300

    chore: Space fix

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ function getNotas() {
   
 	return new Promise((resolve, reject) => {
   	if (!elements.length) return reject(Error('quebrou'));
-    
+ 
     elements.forEach((item, index) => {
     	if (item.checked) collection[item.name] = item.value;
     });
-    
+ 
     resolve(collection);
 	});
 }
