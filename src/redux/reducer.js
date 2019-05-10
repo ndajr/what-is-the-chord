9e0a7cf31f3d907e3580be93b6cd3e35ebbc7e3f
Neemias Junior <neemiasjnr@mac-home.local>
2019-05-10 14:48:25 -0300

    misc: change reducer style

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,26 @@
 import { UPDATE_NOTES, CLEAN_NOTES } from './actions'
 import { traverseInputs, uncheckAllInputs, getCheckedInputs, nodeToArray } from '../helpers'
 
+export const reducer = (state = initialState, action) => {
+  const handler = action.type ? handlers[action.type] : null
+
+  if(!handler) return state
+  return handler(state, action.payload)
+}
+
 export const initialState = {
   notes: [-1, -1, -1, -1, -1, -1]
 }
 
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case CLEAN_NOTES:
-      uncheckAllInputs()
-      return { notes: initialState.notes }
-
-    case UPDATE_NOTES:
-      const inputs = traverseInputs()
-      const checkedInputs = getCheckedInputs(inputs)
-      const notes = nodeToArray(checkedInputs)
-      return { notes }
-      
-    default:
-      return state
+export const handlers = {
+  [CLEAN_NOTES]: (state) => {
+    uncheckAllInputs()
+    return { notes: initialState.notes }
+  },
+  [UPDATE_NOTES]: (state) => {
+    const inputs = traverseInputs()
+    const checkedInputs = getCheckedInputs(inputs)
+    const notes = nodeToArray(checkedInputs)
+    return { notes }
   }
 }
